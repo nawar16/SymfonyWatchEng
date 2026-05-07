40bef033c7ffefe87d4fcc96f471dbdf10952728
nawar16 <nawarmk98@gmail.com>
2026-05-07 20:38:50 +0300

    fix: fix tenant entity missed imports

--- src/Tenancy/Domain/Entity/Tenant.php
+++ src/Tenancy/Domain/Entity/Tenant.php
@@ -6,6 +6,7 @@ use App\Tenancy\Infrastructure\Doctrine\Repository\TenantRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Identity\Domain\Entity\User;
 
 #[ORM\Entity(repositoryClass: TenantRepository::class)]
 #[ORM\Table(name: 'tenant')]
