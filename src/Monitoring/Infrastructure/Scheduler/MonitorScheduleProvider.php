4d320d2c7e7b8654462dab52cad31c7d571e3e58
nawar16 <nawarmk98@gmail.com>
2026-05-11 17:33:45 +0300

    feat: schedule the monitor commands

--- src/Monitoring/Infrastructure/Scheduler/MonitorScheduleProvider.php
+++ src/Monitoring/Infrastructure/Scheduler/MonitorScheduleProvider.php
@@ -5,7 +5,6 @@ namespace App\Monitoring\Infrastructure\Scheduler;
 use App\Monitoring\Application\Command\CheckMonitorCommand;
 use App\Monitoring\Infrastructure\Doctrine\Repository\MonitorRepository;
 use Symfony\Component\Scheduler\Attribute\AsSchedule;
-use Symfony\Component\Scheduler\Generator\MessageGenerator;
 use Symfony\Component\Scheduler\Schedule;
 use Symfony\Component\Scheduler\ScheduleProviderInterface;
 use Symfony\Component\Scheduler\RecurringMessage;
@@ -19,19 +18,22 @@ class MonitorScheduleProvider implements ScheduleProviderInterface
 
     public function getSchedule(): Schedule
     {
-        return (new Schedule())->add(
-            RecurringMessage::every('1 minute', new class($this->monitorRepository) implements MessageGenerator {
-                public function __construct(private MonitorRepository $repo) {}
-                public function getMessages(): iterable
-                {
-                    //TODO: we can add isActive filter herre to increase effeciency and filter unactive monitors
-                    $monitors = $this->repo->findAll();
-
-                    foreach ($monitors as $monitor) {
-                        yield new CheckMonitorCommand($monitor->getId());
-                    }
-                }
-            })
-        );
+            
+        $schedule = new Schedule();
+        $monitors = $this->monitorRepository->findAll();
+        foreach ($monitors as $monitor) {
+            $schedule->add(
+                RecurringMessage::every('1 minute', new CheckMonitorCommand($monitor->getId()))
+            );
+        }
+        return $schedule;
+        // return (new Schedule())->add(
+        //     RecurringMessage::every('1 minute', function() {
+        //         $monitors = $this->monitorRepository->findAll();
+        //         foreach ($monitors as $monitor) {
+        //             yield new CheckMonitorCommand($monitor->getId());
+        //         }
+        //     })
+        // );
     }
 }
